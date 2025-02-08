18751d8e8e24247707b49c342a972ba087d2f489
cool.gitter.choco <gitter@cordovault.com>
2025-02-08 13:55:17 -0600

    yes

--- builds/dev.build.sh
+++ builds/dev.build.sh
@@ -1,1 +1,1 @@
-docker buildx build --push --platform linux/amd64,linux/arm64 --tag cooldockerizer93/spotizerr:dev .
+docker buildx build --no-cache --push --platform linux/amd64,linux/arm64 --tag cooldockerizer93/spotizerr:dev .
--- builds/latest.build.sh
+++ builds/latest.build.sh
@@ -1,1 +1,1 @@
-docker buildx build --push --platform linux/amd64,linux/arm64 --tag cooldockerizer93/spotizerr:latest .
+docker buildx build --no-cache --push --platform linux/amd64,linux/arm64 --tag cooldockerizer93/spotizerr:latest .
