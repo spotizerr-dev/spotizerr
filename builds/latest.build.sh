e1a63633fd675275bf12a11f35f5e89b611e339f
architect.in.git <github@cordovault.com>
2025-04-23 13:00:14 -0600

    Added .env to the setup instructions

--- builds/latest.build.sh
+++ builds/latest.build.sh
@@ -1,1 +1,1 @@
-docker buildx build --push --load --platform linux/amd64,linux/arm64 --build-arg CACHE_BUST=$(date +%s) --tag cooldockerizer93/spotizerr:latest .
+docker buildx build --push --platform linux/amd64,linux/arm64 --build-arg CACHE_BUST=$(date +%s) --tag cooldockerizer93/spotizerr:latest .
